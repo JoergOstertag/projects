b6b81b6062f8ee172201f67f41c91aea61f78e81
Jörg Ostertag <JoergOstertag@users.noreply.github.com>
2020-06-17 23:11:34 +0200

    Put result into svg

--- ESP8266/Wemos-D1-Mini/Distance-vl53l0x-and-Servo/Distance-vl53l0x-and-Servo.ino
+++ ESP8266/Wemos-D1-Mini/Distance-vl53l0x-and-Servo/Distance-vl53l0x-and-Servo.ino
@@ -37,6 +37,7 @@ ESP_WiFiManager ESP_wifiManager("ESP_Configuration");
 #define SERVO_POS_MIN 30
 #define SERVO_POS_MAX 150
 #define SERVO_INCREMENT 5
+#define SERVO_TO_DEGREE .5
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
@@ -164,12 +165,16 @@ void drawGraph() {
   out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
   out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
   out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x += 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+  int y = 0;
+  int x = 0;
+
+  for ( int i = SERVO_POS_MIN ; i <= SERVO_POS_MAX; i += SERVO_INCREMENT) {
+    int y2 = result[i];
+    int x2 = i * SERVO_TO_DEGREE;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x2, 140 - y2);
     out += temp;
     y = y2;
+    x = x2;
   }
   out += "</g>\n</svg>\n";
 
@@ -233,7 +238,11 @@ void loop() {
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
     float dist_cm = measure.RangeMilliMeter / 10.0;
     printStatus(servoPos, dist_cm);
-    result[servoPos] = dist_cm;
+    if ( dist_cm > 800) {
+      result[servoPos] = -1;
+    } else {
+      result[servoPos] = dist_cm;
+    }
   } else {
     result[servoPos] = -1;
     printStatus(servoPos, -1);
@@ -251,9 +260,8 @@ void loop() {
     servoIncrement = SERVO_INCREMENT;
   }
 
-  delay(100);
   myservo.write(servoPos );
-  delay(200);
+  delay(100);
 
   if (ACTIVATE_WEBSERVER) {
     server.handleClient();
