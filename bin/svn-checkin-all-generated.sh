659371912299e7072ac14837331ddf5d3e969a20
Jörg Ostertag <JoergOstertag@users.noreply.github.com>
2020-09-18 11:03:18 +0200

    Add status print and comments

--- bin/svn-checkin-all-generated.sh
+++ bin/svn-checkin-all-generated.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-find . -name "stl" -o -name "README-images.md" -print0 | xargs  -0 svn ci -m"Update generated stl and png"
+echo "Status"
+find . -name "stl" -o -name "README-images.md" -print0 | xargs  -0 svn status
+
+echo
+echo "Checkin ..."
+find . -name "stl" -o -name "README-images.md" -print0 | xargs  -0 svn ci -m'Update generated stl and png'
