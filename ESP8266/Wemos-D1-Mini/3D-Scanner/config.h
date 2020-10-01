ab6d5990edbfd9bcc63adbcd862efeb52e111634
Jörg Ostertag <tweety@ubook>
2020-10-01 11:06:21 +0200

    add define options

--- ESP8266/Wemos-D1-Mini/3D-Scanner/config.h
+++ ESP8266/Wemos-D1-Mini/3D-Scanner/config.h
@@ -6,15 +6,16 @@
 // Servos
 
 // #define SERVO_PCA
-#define PIN_SERVO_AZ D7
-#define PIN_SERVO_EL D8
+//#define PIN_SERVO_AZ D7
+//#define PIN_SERVO_EL D8
 
-// #define PIN_SERVO_AZ D3
-// #define PIN_SERVO_EL D4
+#define PIN_SERVO_AZ D3
+#define PIN_SERVO_EL D4
 
 // ----------------------------------
 // Distance Sensor
-#define USE_DISTANCE_VL53L0X
+// #define USE_DISTANCE_VL53L0X
+// #define _DISTANCE_LIDAR_LITE
 
 /**
    Result storage Handler
