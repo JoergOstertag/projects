49be0173425872d015c9ea8e700b73a9e715e411
Jörg Ostertag <tweety@ubook>
2020-10-20 22:12:02 +0200

    Change the way we count through az/el, extract modules to display

--- ESP8266/Wemos-D1-Mini/3D-Scanner/bin/convert-csv-2-openscad.pl
+++ ESP8266/Wemos-D1-Mini/3D-Scanner/bin/convert-csv-2-openscad.pl
@@ -6,178 +6,229 @@ use Data::Dumper;
 use List::Util qw(min max);
 use Math::Trig;
 
+my $fov = .5;
+my $neigungSensor=45;
 sub writeHeader($){
     my $of = shift;
     print $of "
 // ===========================================================
 // 3D-Scan with Distance Sensor
 // ===========================================================
-fov= 2;
-\n";
+fov= $fov;
+
+// als kugel ==>1 sonst PyramidenSpitz
+drawSphere=1;
+neigungSensor=$neigungSensor;
+
+
+// include <walls.scad>
+
+// drawPolar();
+drawKarth();
+
+";
 }
 
 sub writeModule($){
     my $of = shift;
     
-print $of "\n";
-print $of "\n";
-print $of "\n";
-print $of "\n";
-print $of "module segment(az=0,el=0,dist=20){
-if ( dist > 0 ) {
-    tanFactor=tan(fov);
-    deltaDist= .1;
-    dist1= dist-deltaDist;
+print $of "
 
-      rotate( [el,0,-az] )
-        rotate( [-90,0,0] )
-          translate([0,0,dist1])
-            cylinder( d1= tanFactor*dist1,
-                      d2= tanFactor*dist,
-                       h= deltaDist );
-    }
+module showMarker(x=0,y=0,z=0){
+    translate([x,y,z])
+	color(\"black\")
+	sphere(d=5);
+
 }
 
+module segment(az=0, el=0, dist=20 ) {
+    if ( dist > 0 ) {
+       tanFactor=tan(fov);
+       deltaDist= .1;
+       dist1= dist-deltaDist;
 
+       rotate( [neigungSensor,0,0] )
+	rotate( [el,0,-az] )
+           rotate( [-90,0,0] )
+	      translate([0,0,dist1])
+		if( drawSphere ){
+                   color(\"blue\")
+	              sphere(d=5); 
+                } else {
+                   cylinder( d1= tanFactor*dist1,
+	                     d2= tanFactor*dist,
+	                     h= deltaDist );
+                }
+      }
+}
     ";
 }
 
 
+my $minZ;
 
-my $filenameAll = 'all.scad';
-my $of;
-open($of, '>', $filenameAll)
-    or die "Could not open file '$filenameAll' $!";
 
-writeHeader($of);
-writeModule($of);
-
-foreach my $fileName (@ARGV) {
+sub readCsvFile($){
+    my $fileName = shift;
+    
     print "Import $fileName\n";
+
     open(my $fh, $fileName)
 	or warn "Can't open $fileName: $!";
 
-    my $oFilename= "$fileName.scad";
-    $oFilename =~s/\.csv//;
-    my $of1;
-    open($of1, '>', $oFilename)
-	or die "Could not open file '$oFilename' $!";
-    print "Write $oFilename\n";
-    writeHeader($of1);
-    writeModule($of1);
+    my $values;
 
-    my $line = readline $fh;
+    $minZ   = 100000;
+    
+    my $count=0;
+    my $line = readline $fh; # Header weg
     while ( ! eof($fh) ) {
+	$count++;
 	$line = readline $fh;
 	$line =~ s/\s*$//;
 	my ($az,$el,$dist) = split(/;/,$line);
-	my $outLine="segment(az=$az,	el=$el,	dist=$dist);\n";
-	print $of $outLine;
-	print $of1 $outLine;
+	if ( ! defined($dist) ){
+	    print " Error in line '$line'\n";
+	}
+	$az= $az*1;
+	$el= $el*1;
+	my $az1 = $az * 1.0; # + 90.0;
+	my $el1 = $el * 1.11;
 	
+	# print "($az1,$el1,$dist)\n";
+	my $azRad = $az1 * pi() / 180;
+	my $elRad = $el1 * pi() / 180;
+	
+	my $distFloor= cos($elRad) * $dist;
+	my $z = sin($elRad) * $dist;
+	my $x = cos($azRad) * $distFloor;
+	my $y = sin($azRad) * $distFloor;
+
+	# Rotate 45° down for xyz
+	my $d = sqrt($x*$x + $z*$z);
+	my $alpha = atan($z / $x );
+	$alpha += $neigungSensor;
+	$x = $d * cos($alpha);
+	$z = $d * sin($alpha);
+
+	# Nachkomma Stellen reduzieren
+	$x =sprintf("%6.2f",$x);
+	$y =sprintf("%6.2f",$y);
+	$z =sprintf("%6.2f",$z);
+
+	# if ( $count < 20 )
+	{
+	    $values->{$az}->{$el}={
+		dist => $dist,
+		x    => $x,
+		y    => $y,
+		z    => $z,
+		az   => $az1,
+		el   => $el1,
+	    };
+	};
+	$minZ=min($z,$minZ);
     }
-    
     close($fh);
-    close($of1);
+    return $values;
 }
 
 
-
-
 foreach my $fileName (@ARGV) {
-    print "Import $fileName\n";
+
+    my $values = readCsvFile($fileName);
 
     my $baseFilename= "$fileName";
     $baseFilename =~s/\.csv//;
 
-
-    open(my $fh, $fileName)
-	or warn "Can't open $fileName: $!";
-
-
-    my $values;
-    my $stepAz=2;
-    my $stepEl=2;
-    my $minAz=180;
-    my $minEl=180;
-    my $maxAz=-180;
-    my $maxEl=-180;
-    my $minZ=100000;
+    # Get all possible az/el Values
+    my @azValues = sort { $a <=> $b } keys %{$values};
+    my %elValues;
+    for my $az ( @azValues ) {
+	map { $elValues{$_} = 1 } keys %{$values->{$az}};
+    };
+    my @elValues = sort { $a <=> $b } keys %elValues;
+    #print Dumper ( \@elValues );
+    # print Dumper( \@azValues );
     
-    my $line = readline $fh; # Header weg
-    while ( ! eof($fh) ) {
-	$line = readline $fh;
-	$line =~ s/\s*$//;
-	my ($az,$el,$dist) = split(/;/,$line);
-	$az= $az*1;
-	$el=$el*1;
-	print "($az,$el,$dist)\n";
-	my $azRad = $az * pi() / 180;
-	my $elRad = $el * pi() / 180;
-	
-	my $distFloor= cos($elRad)*$dist;
-	my $z = sin($elRad) * $dist;
-	my $x = cos($azRad) * $distFloor;
-	my $y = sin($azRad) * $distFloor;
-	$values->{$az}->{$el}={
-	    dist=>$dist,
-	    x => $x,
-	    y => $y,
-	    z => $z
-	};
-	$minAz=min($az,$minAz);
-	$maxAz=max($az,$maxAz);
-	$minEl=min($el,$minEl);
-	$maxEl=max($el,$maxEl);
-	$minZ=min($z,$minZ);
-    }
-    close($fh);
-
-
     # ----------------- write scad
     my $of;
-    if ( 0 ){
-	open($of, '>', "$baseFilename-1.scad")
-	    or die "Could not open file '$baseFilename-1.scad' $!";
-	print "Write $baseFilename-1.scad\n";
-	writeHeader($of);
-	writeModule($of);
 
-    for ( my $az=$minAz ; $az<=$maxAz ; $az+=$stepAz ){
-	for ( my $el=$minEl ; $el<=$maxEl ; $el+=$stepEl ){
+    open($of, '>', "$baseFilename.scad")
+	or die "Could not open file '$baseFilename.scad' $!";
+    print " Write $baseFilename.scad\n";
+    writeHeader($of);
+
+    print $of "\n";
+    print $of "minZ=$minZ;\n";
+    print $of "\n";
+    print $of "translate([0,0,-minZ])\n";
+    print $of "// drawPolar();\n";
+    print $of "drawKarth();\n";
+    print $of "\n";
+    
+    writeModule($of);
+
+    print $of "\n";
+    print $of "module drawPolar(){\n";
+    for my $az ( @azValues ) {
+	for my $el ( @elValues ){
+	    if (! defined( $values->{$az}->{$el})){
+		print "   missing $az $el\n";
+	    } else {	    
 	    my $dist = $values->{$az}->{$el}->{dist};
-	    my $outLine="segment(az=$az,	el=$el,	dist=$dist);\n";
+	    my $az1 = $values->{$az}->{$el}->{az};
+	    my $el1 = $values->{$az}->{$el}->{el};
+	    my $outLine="   segment(az=$az1,	el=$el1,	dist=$dist);\n";
 	    print $of $outLine;
 	}
+	}
     }
+    print $of "}\n";
+    print $of "\n";
+
+    
+    print $of "module drawKarth(){\n";
+    for my $az ( @azValues ) {
+	for my $el ( @elValues ){
+	    if (! defined( $values->{$az}->{$el})){
+		print "   missing $az $el\n";
+	    } else {	    
+	    my $x = $values->{$az}->{$el}->{x};
+	    my $y = $values->{$az}->{$el}->{y};
+	    my $z = $values->{$az}->{$el}->{z};
+	    my $outLine="   showMarker($x, $y, $z);\n";
+	    print $of $outLine;
+	}
+	}
+    }
+    print $of "}\n";
+    print $of "\n";
     close($of);
-}
+
 
     # ------------------- write xyzbase
     open($of, '>', "$baseFilename.xyz")
 	or die "Could not open file '$baseFilename.xyz' $!";
-    print "Write $baseFilename.xyz\n";
+    print " Write $baseFilename.xyz\n";
 
-    print "Az: $minAz $maxAz += $stepAz\n";
-    print "El: $minEl $maxEl += $stepEl\n";
-    for ( my $az=$minAz ; $az<=$maxAz ; $az += $stepAz ){
-	for ( my $el=$minEl ; $el<=$maxEl ; $el += $stepEl ){
-	    print " values: $az $el: ";
+    for my $az ( @azValues ) {
+	for my $el ( @elValues ){
 	    if (! defined( $values->{$az}->{$el})){
-		print "missing\n";
+		print "   missing $az $el\n";
 	    } else {	    
 		my $x = $values->{$az}->{$el}->{x};
 		my $y = $values->{$az}->{$el}->{y};
 		my $z = $values->{$az}->{$el}->{z} - $minZ;
-		my $outLine= sprintf("%12.8f %12.8f %12.8f\n",$x, $y, $z);
-		print "$outLine\n";
+		my $outLine= sprintf("%12.3f %12.3f %12.3f",$x, $y, $z);
+		# print "$outLine\n";
 		print $of $outLine;
 	    }
 	}
     }
     close($of);
-    
-#    print Dumper(\$values);
+
+    #    print Dumper(\$values);
 }
 
 
